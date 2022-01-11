de2bfc4634280b5ac0e2d5fbf096088220a2655b
Abhishek-M-Rao <70395274+Abhishek-M-Rao@users.noreply.github.com>
2022-01-11 10:23:12 -0600

    learning classes on python

---
+++ learning_classes.py
@@ -0,0 +1,5 @@
+print("hello world!")
+# class Robot:
+#     def introduce_self(self):
+#         print("My name is " + self.name) #this is like c "self" is kinda like "this" like pointer to the object
+
